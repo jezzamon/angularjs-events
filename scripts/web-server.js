aa2cabd760d4f38d300a753da067523439a9f9a4
jezzamondev <jezzamondev@gmail.com>
2017-11-12 17:25:08 -0500

    ROUTING - enable html5 routing - no hashes

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -12,5 +12,8 @@ app.get('/data/event/:id', events.get);
 app.get('/data/event/', events.getAll);
 app.post('/data/event/:id', events.save);
 
+//will need to add this fallback if the routes above are not hit (need this for $locationProvider.html5Mode )
+app.get('*', function(req, res) { res.sendFile(rootPath + '/app/index.html'); });
+
 app.listen(8090);
 console.log('listening to port 8000');
